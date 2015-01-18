541b22c03e011e11d50a059dbecaa6ddec43c868
Greg Ziegan <grz5@case.edu>
2015-01-17 23:42:12 -0500

    Debug = False if not stated otherwise

--- anovelmous_app/settings.py
+++ anovelmous_app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', False)
 
 TEMPLATE_DEBUG = True
 
