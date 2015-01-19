1736330cc012dda4fcd09f3e86a035c36fdc6cd0
Greg Ziegan <grz5@case.edu>
2015-01-18 20:35:44 -0500

    template debug and added placeholder staticfiles

--- anovelmous_app/settings.py
+++ anovelmous_app/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = os.environ.get('TEMPLATE_DEBUG', False)
 
 ALLOWED_HOSTS = ['.heroku.com', '.anovelmous.com']
 
