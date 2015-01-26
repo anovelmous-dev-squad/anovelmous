ead79feb60f283f5b3cca493c84005c1956b6c1d
Greg Ziegan <grz5@case.edu>
2015-01-26 00:05:42 -0500

    added filtering to several models

--- anovelmous_app/settings.py
+++ anovelmous_app/settings.py
@@ -46,6 +46,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
     ),
+    'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',),
     'PAGINATE_BY': 100,
     'PAGINATE_BY_PARAM': 'page_size',
 }
