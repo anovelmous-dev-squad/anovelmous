fda792ce680ec2550a23e3a364275e6685dece1a
Greg Ziegan <grz5@case.edu>
2015-02-11 18:11:34 -0500

    add huey and redis

--- anovelmous_app/settings.py
+++ anovelmous_app/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', False)
 
-TEMPLATE_DEBUG = os.environ.get('TEMPLATE_DEBUG', False)
+TEMPLATE_DEBUG = DEBUG
 
 ALLOWED_HOSTS = []
 
