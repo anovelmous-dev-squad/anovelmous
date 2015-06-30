459d56eac3a8104d2837769d43776230dc541032
Greg Ziegan <grz5@case.edu>
2015-06-30 19:20:27 +1200

    manual mapping for contributors

--- api/migrations/0008_auto_20150630_0426.py
+++ api/migrations/0008_auto_20150630_0426.py
@@ -5,18 +5,7 @@ from django.db import models, migrations
 import uuid
 
 
-def migrate_default_contributor(apps, schema_editor):
-    contributor_model = apps.get_model('api', 'contributor')
-    user_1 = apps.get_model('auth', 'user').objects.all()[0]
-    c = contributor_model(id=user_1.id, username=user_1.username, email=user_1.email,
-                          date_joined=user_1.date_joined, client_id=uuid.uuid4())
-    c.save()
-
 class Migration(migrations.Migration):
-
-    run_before = [
-        migrations.RunPython(migrate_default_contributor)
-    ]
 
     dependencies = [
         ('api', '0007_contributor_guild'),
