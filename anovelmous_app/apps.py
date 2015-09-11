33c42c3f126a063cbfe99998cfb86ae4340339a5
Greg Ziegan <greg.ziegan@gmail.com>
2015-09-11 01:54:50 -0400

    workaround created for an initially empty database when trying to create a token cache on start

--- anovelmous_app/apps.py
+++ anovelmous_app/apps.py
@@ -5,13 +5,17 @@ from grammar import GrammarFilter
 from django.core.cache import cache
 from .settings import NLTK_DATA_ABS_PATH
 
-
 class APIConfig(AppConfig):
     name = 'api'
     verbose_name = 'Anovelmous Application'
 
     def ready(self):
         token_model = self.get_model('Token')
-        full_vocabulary = list(token_model.objects.all().values_list('content', flat=True))
+        try:
+            full_vocabulary = list(token_model.objects.all().values_list('content', flat=True))
+        except Exception as e:
+            # Need to create db first, psycopg's ProgrammingError exception does not bubble up
+            return
+
         gf = GrammarFilter(vocabulary=full_vocabulary, nltk_data_path=NLTK_DATA_ABS_PATH)
         cache.set('grammar_filter', gf)
